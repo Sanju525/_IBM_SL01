60860e1c07fb2d89b702c8d8f142c0f7bb83a97a
Seelam Sanjay Ganesh <51526260+Sanju525@users.noreply.github.com>
2020-06-12 22:07:49 +0530

    Update simple-regression.py

--- simple-regression.py
+++ simple-regression.py
@@ -89,60 +89,8 @@ st.info("Hello, check the box below to show the model code!")
 
 @st.cache
 def codeData():
-    data00 = "\
-    import pandas as pd\n\
-    import matplotlib.pyplot as plt\n\
-    import numpy as np\n\
-    from sklearn import linear_model\n\
-    \n\
-    \n\
-    df = pd.read_csv('FuelConsumption.csv')\n\
-    c_df = df[['ENGINESIZE', 'CYLINDERS', 'FUELCONSUMPTION_COMB', 'CO2EMISSIONS']]\n\
-        \n\
-    plt.scatter(c_df['ENGINESIZE'], c_df['CO2EMISSIONS'])\n\
-    plt.title('Scatter plot of Enginesize and CO2 Emission')\n\
-    plt.xlabel('ENGINE SIZE')\n\
-    plt.ylabel('CO2 EMISSIONS')\n\
-    plt.show()\n\
-        \n\
-    def train_test(df0):\n\
-        msk = np.random.rand(len(df0)) < 0.8\n\
-        train0 = c_df[msk]\n\
-        test0 = c_df[~msk]\n\
-        return train0, test0\n\
-        \n\
-    train, test = train_test(df)\n\
-        \n\
-    model = linear_model.LinearRegression()\n\
-    x = np.asanyarray(train[['ENGINESIZE']])\n\
-    y = np.asanyarray(train[['CO2EMISSIONS']])\n\
-    model.fit(x, y)\n\
-        \n\
-    test_y = np.asanyarray(test[['CO2EMISSIONS']])  # Score of prediction\n\
-        \n\
-    inputVal = float(input('Enter a EngineSize value for testing the model from Test Data'))\n\
-    test_x = np.asanyarray(test[['ENGINESIZE']])\n\
-        \n\
-    val = np.array(inputVal)\n\
-        \n\
-    new_val = val.reshape(1, 1)\n\
-        \n\
-    predicted_val = model.predict(new_val)\n\
-    print('Predicted value for '+str(new_val.item())+' is : '+str(predicted_val.item()))\n\
-    \n\
-    test_y_ = model.predict(test_x)\n\
-        \n\
-    from sklearn.metrics import r2_score\n\
-        \n\
-    score = r2_score(test_y_, test_y)\n\
-        \n\
-    plt.scatter(train['ENGINESIZE'],train['CO2EMISSIONS'], color= 'green')\n\
-    plt.title('Graph of Linear Regression')\n\
-    plt.xlabel('ENGINE SIZE')\n\
-    plt.ylabel('CO2 EMISSIONS')\n\
-    plt.plot(test['ENGINESIZE'], test_y_, color='red')\n\
-    plt.show()\n\
-    print('Prediction Accuracy: '+str(score))"
+    file_object = open('modelC.py,'r')
+    data00 = file_object.read()
     return data00
 
 
